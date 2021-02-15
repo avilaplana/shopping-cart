f493be913a5dfc004374e6d9e904dae827358516
Alvaro Vilaplana Garcia <alvaro.vilaplana@heimstaden.com>
2021-02-15 23:21:18 +0000

    Second problem - TDD and solution

--- src/main/scala/hmrc/ShoppingCart.scala
+++ src/main/scala/hmrc/ShoppingCart.scala
@@ -1,7 +1,8 @@
 package hmrc
 
-import hmrc.domain.Item
 import cats.Monoid
+import hmrc.domain.Item.Apple
+import hmrc.domain.{Item, Offer}
 
 object domain {
   sealed trait Item {
@@ -11,6 +12,13 @@ object domain {
   object Item {
     case class Apple(price: BigDecimal)  extends Item
     case class Orange(price: BigDecimal) extends Item
+  }
+
+  case class Offer(buy: Int, pay: Int) {
+    def applyTo(items: List[Item]): List[Item] = {
+      val sizeToPay = (items.size / buy) * pay + items.size % buy
+      items.take(sizeToPay)
+    }
   }
 }
 
@@ -22,6 +30,23 @@ object PriceMonoid extends Monoid[BigDecimal] {
 object ShoppingCart {
   def checkout(items: List[Item]): String = {
     val totalCost = items.foldRight(PriceMonoid.empty) {
+      case (item, priceAcc) => PriceMonoid.combine(item.price, priceAcc)
+    }
+    totalCost.print
+  }
+}
+
+object ShoppingCartWithOffers {
+
+  val appleOffer  = Offer(buy = 2, pay = 1)
+  val orangeOffer = Offer(buy = 3, pay = 2)
+
+  def checkout(items: List[Item]): String = {
+    val (apples, oranges) = items.partition(_.isInstanceOf[Apple])
+    val applesToPay       = appleOffer.applyTo(apples)
+    val orangesToPay      = orangeOffer.applyTo(oranges)
+
+    val totalCost = (applesToPay ++ orangesToPay).foldRight(PriceMonoid.empty) {
       case (item, priceAcc) => PriceMonoid.combine(item.price, priceAcc)
     }
     totalCost.print
